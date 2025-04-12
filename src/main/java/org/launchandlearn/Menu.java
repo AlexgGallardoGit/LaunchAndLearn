fee6df7e11d6c4feaac96eef53456022bacb4bc4
Pablo <jupagogo12@yahoo.com>
2025-04-12 15:43:51 -0400

    changed Menu, still have to set the score,etc

--- src/main/java/org/launchandlearn/Menu.java
+++ src/main/java/org/launchandlearn/Menu.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Menu extends Application {
@@ -20,10 +21,18 @@ public class Menu extends Application {
     }
 
     public void show(Stage stage, String grade) {
+        // Screen size variables
+        double screenWidth = Screen.getPrimary().getBounds().getWidth();
+        double screenHeight = Screen.getPrimary().getBounds().getHeight();
+        //int screenWidth = 1080;
+        //int screenHeight = 720;
+
+        // Fullscreen toggle
+        boolean fullscreen = false; // set to true to go fullscreen
+
         // Grade display label
         Label gradeLabel = new Label("Your Grade: " + grade);
         gradeLabel.setStyle("-fx-font-size: 45px; -fx-text-fill: darkgreen;");
-
 
         // Next button
         Button nextButton = new Button("Next");
@@ -58,15 +67,19 @@ public class Menu extends Application {
         });
 
         // Layout
-        VBox layout = new VBox(30);
-        layout.getChildren().addAll(gradeLabel, nextButton, restartButton,exitButton);
+        VBox layout = new VBox(20);
+        layout.getChildren().addAll(gradeLabel, nextButton, restartButton, exitButton);
         layout.setAlignment(Pos.CENTER);
         layout.setStyle("-fx-background-color: white;");
 
-        // Scene setup
-        Scene endScene = new Scene(layout, 520, 350);
+        // Scene setup using screenWidth and screenHeight
+        Scene endScene = new Scene(layout, screenWidth * 0.4, screenHeight * 0.4);
         stage.setScene(endScene);
         stage.setTitle("Menu");
+
+        // Set fullscreen if needed
+        stage.setFullScreen(fullscreen);
+
         stage.show();
     }
 }
